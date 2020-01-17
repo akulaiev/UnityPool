d014138c6758f4b238d652c3e669602bfabc3574
akulaiev <kulaeva.a@ukr.net>
2020-01-17 18:49:36 +0200

    started day03

---
+++ Day03/Assets/Scripts/dragDrop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class dragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler,
+IDragHandler {
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log("OnBeginDrag");
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log("OnEndDrag");
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		Debug.Log("OnDrag");
+	}
+
+	public void OnPointerDown(PointerEventData eventData) {
+		Debug.Log("OnPointerDown");
+	}
+	
+}
